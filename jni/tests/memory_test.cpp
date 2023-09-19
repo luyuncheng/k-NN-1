aa14cfa1006353b357fe0319c59aeebcecedf6b4
luyuncheng <luyuncheng@bytedance.com>
2023-09-19 14:04:01 +0800

    Update and rename faiss_memory_test.cpp to memory_test.cpp
    Signed-off-by: luyuncheng <luyuncheng@bytedance.com>

--- jni/tests/memory_test.cpp
+++ jni/tests/memory_test.cpp
@@ -51,21 +51,17 @@ TEST(FaissHNSWIndexMemoryTest, BasicAssertions) {
     }
 
     free(data_load);
-    malloc_trim(0);
 
     std::string indexPath = "tmp/FaissHNSWIndexMemoryTest.faiss";
     std::string spaceType = knn_jni::L2;
     std::string index_description = "HNSW32,Flat";  
     int thread_num = 7;
-    //int efConstruction = 512;
-    //int efSearch = 512;
+    
     std::unordered_map<std::string, jobject> parametersMap;
     parametersMap[knn_jni::SPACE_TYPE] = (jobject)&spaceType;
     parametersMap[knn_jni::INDEX_DESCRIPTION] = (jobject)&index_description;
     parametersMap[knn_jni::INDEX_THREAD_QUANTITY] = (jobject)&thread_num;
-    //parametersMap[knn_jni::EF_CONSTRUCTION] = (jobject)&efConstruction;
-    //parametersMap[knn_jni::EF_SEARCH] = (jobject)&efSearch;
-
+    
     // Set up jni
     JNIEnv *jniEnv = nullptr;
     NiceMock<test_util::MockJNIUtil> mockJNIUtil;
@@ -81,22 +77,18 @@ TEST(FaissHNSWIndexMemoryTest, BasicAssertions) {
             reinterpret_cast<jobjectArray>(&vectors), (jstring)&indexPath,
             (jobject)&parametersMap);
 
-    // Make sure index can be loaded
-    // std::unique_ptr<faiss::Index> index(test_util::FaissLoadIndex(indexPath));
-
     // Clean up
     ids.clear();
     ids.shrink_to_fit();
     vectors.clear();
     vectors.shrink_to_fit();
 
-    malloc_trim(0);
     size_t mem_usage = faiss::get_mem_usage_kb() / (1 << 10);
 
     GTEST_COUT<<"======Memory Usage:[" << mem_usage << "mb]======" << std::endl;
 }
 
-TEST(LIBHNSWIndexMemoryTest, BasicAssertions) {
+TEST(NmslibHNSWIndexMemoryTest, BasicAssertions) {
 
     similarity::initLibrary();
     char dataset[] = "dataset/sift/sift_base.fvecs";
@@ -123,9 +115,8 @@ TEST(LIBHNSWIndexMemoryTest, BasicAssertions) {
     }
 
     free(data_load);
-    malloc_trim(0);
 
-    std::string indexPath = "tmp/LibHNSWIndexMemoryTest.faiss";
+    std::string indexPath = "tmp/NmslibHNSWIndexMemoryTest.hnsw";
     std::string spaceType = knn_jni::L2;
     int thread_num = 7;
     int efConstruction = 512;
@@ -156,9 +147,6 @@ TEST(LIBHNSWIndexMemoryTest, BasicAssertions) {
             &mockJNIUtil, jniEnv, reinterpret_cast<jintArray>(&ids),
             reinterpret_cast<jobjectArray>(&vectors), (jstring)&indexPath,
             (jobject)&parametersMap);
-
-    // Make sure index can be loaded
-    // std::unique_ptr<faiss::Index> index(test_util::FaissLoadIndex(indexPath));
 
     // Clean up
     ids.clear();
