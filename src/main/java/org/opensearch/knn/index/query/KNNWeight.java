146a94770c3f2658ec6950e47bf04a075a6637e7
luyuncheng <luyuncheng@bytedance.com>
2024-08-06 14:09:41 +0800

    Add DocValuesProducer for KNNNativeEngine
    Signed-off-by: luyuncheng <luyuncheng@bytedance.com>

--- src/main/java/org/opensearch/knn/index/query/KNNWeight.java
+++ src/main/java/org/opensearch/knn/index/query/KNNWeight.java
@@ -273,6 +273,7 @@ public class KNNWeight extends Weight {
         FilterIdsSelector.FilterIdsSelectorType filterType = filterIdsSelector.getFilterType();
         // Now that we have the allocation, we need to readLock it
         indexAllocation.readLock();
+        indexAllocation.incRef();
         try {
             if (indexAllocation.isClosed()) {
                 throw new RuntimeException("Index has already been closed");
@@ -320,6 +321,7 @@ public class KNNWeight extends Weight {
             throw new RuntimeException(e);
         } finally {
             indexAllocation.readUnlock();
+            indexAllocation.decRef();
         }
 
         /*
